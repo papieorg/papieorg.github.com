63406cb28b16cba8de5ac690c709979c195cfe09
Bill Cortright <bcortrig@gmail.com>
2012-07-30 21:13:34 -0700

    Changes from the orginal pull

---
+++ SubMenuLearnMore.js
@@ -0,0 +1,18 @@
+<!--To Create Menus
+function mmLoadMenus() {
+  if (window.mm_menu_0324095466_0) return;
+
+   
+      window.mm_menu_0324095466_0 = new Menu("root",130,22,"Arial, Helvetica, sans-serif",10,"#505050","#FFFFFF","#CECECF","#386FAD","left","middle",3,0,1000,-5,7,true,false,true,2,false,false);
+        mm_menu_0324095466_0.addMenuItem("Elementary&nbsp;Schools","location='02_OurImpact/ElementaryTargets.html'");
+        mm_menu_0324095466_0.addMenuItem("Middle&nbsp;Schools","location='02_OurImpact/MiddleTargets.html'");
+        mm_menu_0324095466_0.addMenuItem("High&nbsp;Schools","location='02_OurImpact/HighTargets.html'");
+         mm_menu_0324095466_0.hideOnMouseOut=true;
+         mm_menu_0324095466_0.bgColor='#555555';
+         mm_menu_0324095466_0.menuBorder=1;
+         mm_menu_0324095466_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_0324095466_0.menuBorderBgColor='#777777';
+
+
+mm_menu_0324095466_0.writeMenus();
+} // mmLoadMenus()
