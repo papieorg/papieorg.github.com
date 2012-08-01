084a131db51a9f3ce4dd4d95ec6e0df2d76c3e03
Rebecca Shen <rebecca@emenar.com>
2012-07-31 22:52:03 -0700

    Fixes per Kathy's changes

--- SubMenuBlue.js
+++ SubMenuBlue.js
@@ -5,7 +5,7 @@ function mmLoadMenus() {
   mm_menu_0324093759_0.addMenuItem("Mission","location='./01_About/Mission.html'");
   mm_menu_0324093759_0.addMenuItem("History","location='01_About/History.html'");
   mm_menu_0324093759_0.addMenuItem("Financial&nbsp;Performance","location='01_About/Financials.html'");
-  mm_menu_0324093759_0.addMenuItem("Report&nbsp;to&nbsp;the&nbsp;Community","location= 'PiEimages/PiE_PDFs/PAPiE_annual_2010.pdf'");
+  mm_menu_0324093759_0.addMenuItem("Report&nbsp;to&nbsp;the&nbsp;Community","location= 'PiEimages/PiE_PDFs/PiE_Annual_2011_Short.pdf'");
   mm_menu_0324093759_0.addMenuItem("Board&nbsp;of&nbsp;Directors","location='01_About/BOD.html'");
   mm_menu_0324093759_0.addMenuItem("Advisory&nbsp;Council","location='01_About/AdvisoryCouncil.html'");
   mm_menu_0324093759_0.addMenuItem("Volunteers&nbsp;&amp;&nbsp;Staff","location='01_About/VolunteersStaff.html'");
